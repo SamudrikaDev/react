0c4f84f420b964b45f441665845df9d3a760329f
SamudrikaDev <samudrikadev@gmail.com>
2024-01-19 08:38:49 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,34 @@
+
+// Single div rendering
+// const heading = React.createElement("h1", {id:"head1"}, "Hello World Maa sarala");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// ***** nested html rendering ****/
+
+
+// Create element
+const parent = React.createElement
+    (
+        "div",
+        { id: "parent" },
+        React.createElement(
+            "div",
+            { id: "child" },
+            [React.createElement(
+                "h1",
+                {id : "head1"},
+                "Namaste Maa Sarala"
+            ),
+            React.createElement(
+                "h2",
+                {id : "head2"},
+                "Namaste Maa Sarala h2"
+            )
+            ]
+        )
+    );
+
+// create root
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
